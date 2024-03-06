cacca0524bdecf3204f43d0f04a653db22cf293d
bastienyoussfi <bastien.youssfi@gmail.com>
2024-03-06 19:56:02 +0900

    Organized reps

--- routes/web/home.js
+++ routes/web/home.js
@@ -3,12 +3,15 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-    //console.log("Hello I'm on the main page here");
-    res.render("pages/index");
+    res.render("pages/");
 })
 
 router.get("/home", function(req, res){
     res.render("pages/home");
+})
+
+router.get("/about", function(req, res){
+    res.render(("pages/about"));
 })
 
 module.exports = router;
