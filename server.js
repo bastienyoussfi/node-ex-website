c31d084f8c8c104ed94f2729a062509c6141be1d
bastienyoussfi <bastien.youssfi@gmail.com>
2024-03-06 12:05:32 +0900

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+// resume page
+app.get('/resume', function(req, res) {
+  res.render('pages/resume');
+});
+
+// contact page
+app.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
