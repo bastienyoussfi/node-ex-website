3e55da69ca66223e475faeeae30d0453d70d4d3e
bastienyoussfi <bastien.youssfi@gmail.com>
2024-03-07 00:11:31 +0900

    Log in and Sign up

--- routes/web/home.js
+++ routes/web/home.js
@@ -11,7 +11,15 @@ router.get("/home", function(req, res){
 })
 
 router.get("/about", function(req, res){
-    res.render(("pages/about"));
+    res.render("pages/about");
+})
+
+router.get("/login", function(req, res){
+    res.render("pages/login.ejs");
+})
+
+router.get("/signup", function(req, res){
+    res.render("pages/signup.ejs");
 })
 
 module.exports = router;
